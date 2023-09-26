7859fae71ac1b8139ae1c081559548ec794a0284
Tien Cong <84218037+conggalam12@users.noreply.github.com>
2023-09-26 16:31:29 +0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,94 @@
+from facenet_pytorch import InceptionResnetV1, fixed_image_standardization, training
+import torch
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from torch import optim
+from torch.optim.lr_scheduler import MultiStepLR
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+from train_mtcnn import croped_image
+import numpy as np
+import os
+
+
+data_dir = "img"
+workers = 0 if os.name == 'nt' else 8
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+resnet = InceptionResnetV1(
+    classify=True,
+    pretrained='vggface2',
+    num_classes=31
+).to(device)
+
+optimizer = optim.Adam(resnet.parameters(), lr=0.001)
+scheduler = MultiStepLR(optimizer, [5, 10])
+batch_size = 32
+epochs = 100
+
+
+trans = transforms.Compose([
+    np.float32,
+    transforms.ToTensor(),
+    fixed_image_standardization
+])
+dataset = datasets.ImageFolder(data_dir + '_cropped', transform=trans)
+img_inds = np.arange(len(dataset))
+np.random.shuffle(img_inds)
+train_inds = img_inds[:int(0.8 * len(img_inds))]
+val_inds = img_inds[int(0.8 * len(img_inds)):]
+
+
+train_loader = DataLoader(
+    dataset,
+    num_workers=workers,
+    batch_size=batch_size,
+    sampler=SubsetRandomSampler(train_inds)
+)
+val_loader = DataLoader(
+    dataset,
+    num_workers=workers,
+    batch_size=batch_size,
+    sampler=SubsetRandomSampler(val_inds)
+)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+metrics = {
+    'fps': training.BatchTimer(),
+    'acc': training.accuracy
+}
+
+writer = SummaryWriter()
+writer.iteration, writer.interval = 0, 10
+
+print('\n\nInitial')
+print('-' * 10)
+resnet.eval()
+training.pass_epoch(
+    resnet, loss_fn, val_loader,
+    batch_metrics=metrics, show_running=True, device=device,
+    writer=writer
+)
+
+for epoch in range(epochs):
+    print('\nEpoch {}/{}'.format(epoch + 1, epochs))
+    print('-' * 10)
+
+    resnet.train()
+    training.pass_epoch(
+        resnet, loss_fn, train_loader, optimizer, scheduler,
+        batch_metrics=metrics, show_running=True, device=device,
+        writer=writer
+    )
+
+    resnet.eval()
+    training.pass_epoch(
+        resnet, loss_fn, val_loader,
+        batch_metrics=metrics, show_running=True, device=device,
+        writer=writer
+    )
+torch.save(resnet.state_dict(), "resnet.pth")
+writer.close()
+
+
+
+
---
+++ train_mtcnn.py
@@ -0,0 +1,39 @@
+from facenet_pytorch import MTCNN, training
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import os
+
+def load_dataset(data_dir):
+    workers = 0 if os.name == 'nt' else 8
+    dataset = datasets.ImageFolder(data_dir, transform=transforms.Resize((512, 512)))
+    dataset.samples = [
+        (p, p.replace(data_dir, data_dir + '_cropped'))
+            for p, _ in dataset.samples
+    ]
+            
+    loader = DataLoader(
+        dataset,
+        num_workers=workers,
+        batch_size=32,
+        collate_fn=training.collate_pil
+    )
+    return loader
+
+def load_model(device):
+    mtcnn = MTCNN(
+    image_size=160, margin=0, min_face_size=20,
+    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+    device=device
+    )
+    return mtcnn
+
+def croped_image(data_dir_path):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print('Running on device: {}'.format(device))
+    mtcnn = load_model(device)
+    loader = load_dataset(data_dir_path)
+    for i, (x, y) in enumerate(loader):
+        mtcnn(x, save_path=y)
+        print('\rBatch {} of {}'.format(i + 1, len(loader)), end='')
+    del mtcnn
